649cff7e40975ea11b01c2f96a7df7971e814b46
lucax88x <lucax88x@gmail.ch>
2024-11-05 18:00:55 +0100

    updates

--- dot_config/nvim/lua/lt/neovide.lua
+++ dot_config/nvim/lua/lt/neovide.lua
@@ -6,4 +6,29 @@ if vim.g.neovide then
   -- vim.g.neovide_cursor_vfx_mode = "railgun"
   -- vim.g.neovide_cursor_vfx_particle_speed = 10.0
   -- vim.g.neovide_cursor_vfx_particle_lifetime = 3.2
+  --
+  --
+  local map = vim.keymap.set
+
+  local function neovideScale(amount)
+    local temp = vim.g.neovide_scale_factor + amount
+
+    if temp < 0.5 then
+      return
+    end
+
+    vim.g.neovide_scale_factor = temp
+  end
+
+  map("n", "<C-=>", function()
+    neovideScale(0.1)
+  end)
+
+  map("n", "<C-->", function()
+    neovideScale(-0.1)
+  end)
+
+  map("n", "<C-0>", function()
+    vim.g.neovide_scale_factor = 1
+  end)
 end
